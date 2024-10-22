fe8778ec4bd170ee075731fba9ccca4e970f71f6
renata tsedenova <recedenova@gmail.com>
2024-10-22 20:07:39 +0400

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: { // [!code ++]
+        serverComponentsExternalPackages: ['grammy'], // [!code ++]
+      }, // [!code ++]
+};
 
 export default nextConfig;
